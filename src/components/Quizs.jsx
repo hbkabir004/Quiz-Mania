f73978bd98e58373e86937a12ae92f2626c50925
ABIR <hbkabir004@gmail.com>
2022-10-12 14:32:53 +0600

    Final Commit

--- src/components/Quizs.jsx
+++ src/components/Quizs.jsx
@@ -10,8 +10,8 @@ const Quizs = () => {
     return (
         <div>
             <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-              <h2 className="inline-block px-3 py-px mb-4 text-xl font-semibold tracking-wider text-deep-purple-accent-400 uppercase rounded-full bg-yellow-200 ">
-                {data.name}
+              <h2 className="inline-block px-3 py-px mb-4 text-2xl font-semibold tracking-wider text-deep-purple-accent-400 uppercase rounded-full bg-yellow-200 ">
+                <span className='text-gray-700'>Quiz For </span>{data.name}
               </h2>
             </div>
             <div className='grid lg:grid-cols-3 sm:grid-cols-2 lg:mx-72 lg:my-10 gap-10'>
